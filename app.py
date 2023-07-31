283861434127b048988dbe4a7b4ce2bb0b7fc101
mojosenpai <mojosamaxiii@gmail.com>
2023-07-31 19:29:44 +0330

    added authentication

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thesecretkey'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 with app.app_context():
     db = SQLAlchemy(app)
+    bcrypt = Bcrypt(app)
 
 post_tag = db.Table('post_tag',
                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
@@ -23,7 +34,7 @@ class Post(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     labels = db.relationship('Tag', secondary='post_tag', backref='posts_labeled')
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     username = db.Column(db.String(50), nullable=False, unique=True)
@@ -35,13 +46,72 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
-@app.route('/')
-def home():
-    return 'Hello world!'
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=6, max=20)
+    ], render_kw={
+        'placeholder': 'username...'
+    })
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=6, max=20)
+    ], render_kw={
+        'placeholder': 'password...'
+    })
+    submit = SubmitField('Login')
 
-@app.route('/info')
-def info():
-    return 'This is a website written in Flask'
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=6, max=20)
+    ], render_kw={
+        'placeholder': 'username...'
+    })
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=6, max=20)
+    ], render_kw={
+        'placeholder': 'password...'
+    })
+    submit = SubmitField('Register')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(name=form.username.data, username=form.username.data, password=hashed_password, role='Author')
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
